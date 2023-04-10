a92c1ca0b85ebc01102aaad522f64bbb96af0313
Alexander Baron <alexander.baron1@rwth-aachen.de>
2023-04-10 13:01:34 +0200

    Load blacklist params from CLI

--- src/main.rs
+++ src/main.rs
@@ -9,20 +9,34 @@ use url::Url;
 #[derive(Parser, Debug)]
 struct Args {
     source: Url,
+
+    // Blacklist rules
+    #[clap(short, long)]
+    blacklist: Vec<String>,
 }
 
 fn main() {
-    let mut blacklist = HashMap::<String, Vec<Regex>>::new();
-    blacklist.insert(
-        "SUMMARY".to_string(),
-        vec![Regex::new(r"^.*TU.*$").unwrap()],
-    );
-
     let args = Args::parse();
+
     let source = reqwest::blocking::get(args.source).unwrap();
     let source = source.text().unwrap();
 
     let reader = ical::IcalParser::new(BufReader::new(source.as_bytes()));
+
+    let mut blacklist = HashMap::<String, Vec<Regex>>::new();
+    args.blacklist.iter().for_each(|b| {
+        let mut split = b.splitn(2, '=');
+        let key = split.next().unwrap();
+        let value = split.next().unwrap();
+
+        let value = Regex::new(value).unwrap();
+
+        if let Some(v) = blacklist.get_mut(key) {
+            v.push(value);
+        } else {
+            blacklist.insert(key.to_string(), vec![value]);
+        }
+    });
 
     for calendar in reader {
         let calendar = calendar.unwrap();
