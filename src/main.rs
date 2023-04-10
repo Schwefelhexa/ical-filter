5649c75cec68a4db0affd0598c89822a2beffa7b
Alexander Baron <alexander.baron1@rwth-aachen.de>
2023-04-10 12:44:38 +0200

    Generate filtered ICS

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, io::BufWriter};
 use std::fs::File;
 use std::io::BufReader;
 
+use ics::{Event, ICalendar, components::Property};
 use regex::Regex;
 
 fn main() {
@@ -30,22 +31,43 @@ fn main() {
         });
         let events = events.collect::<Vec<_>>();
 
-        // filter events
+        let cal_version = calendar
+            .properties
+            .iter()
+            .find(|p| p.name == "VERSION")
+            .unwrap()
+            .value
+            .clone()
+            .unwrap();
+        let prod_id = calendar
+            .properties
+            .iter()
+            .find(|p| p.name == "PRODID")
+            .unwrap()
+            .value
+            .clone()
+            .unwrap();
 
-        for ev in &events {
-            println!(
-                "{}",
-                ev.properties
-                    .iter()
-                    .filter(|p| p.name == "SUMMARY")
-                    .next()
-                    .unwrap()
-                    .value
-                    .clone()
-                    .unwrap()
-            );
-        }
+        let mut output_calendar = ICalendar::new(cal_version, prod_id);
 
-        println!("\nCount: {}/{}", events.len(), calendar.events.len())
+        events.iter().for_each(|e| {
+            let props: HashMap<String, String> = e
+                .properties
+                .iter()
+                .map(|p| (p.name.clone(), p.value.clone().unwrap()))
+                .collect();
+
+            let mut output_event =
+                Event::new(props.get("UID").unwrap().clone(), props.get("DTSTAMP").unwrap().clone());
+
+            e.properties.iter().for_each(|p| {
+                output_event.push(Property::new(p.name.clone(), p.value.clone().unwrap()));
+            });
+
+            output_calendar.add_event(output_event);
+        });
+
+        output_calendar.write(std::io::stdout()).unwrap();
+
     }
 }
