1a70f72deb16e5fde8acb43ff4d33d0f7858f085
Alexander Baron <alexander.baron1@rwth-aachen.de>
2023-04-10 11:34:20 +0200

    ICAL parser setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::fs::File;
+use std::io::BufReader;
+
 fn main() {
-    println!("Hello, world!");
+    let buf = BufReader::new(File::open("/tmp/component.ics").unwrap());
+
+    let reader = ical::IcalParser::new(buf);
+
+    for calendar in reader {
+        println!("{} events", calendar.unwrap().events.len());
+    }
 }
+
