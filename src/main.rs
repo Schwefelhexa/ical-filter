e29a83b44cad5ace7189f9f0b7df2796831ba8b7
Alexander Baron <alexander.baron1@rwth-aachen.de>
2023-04-11 15:59:19 +0200

    Remove all unwraps

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,15 @@ use clap::Parser;
 use itertools::Itertools;
 use std::net::SocketAddr;
 
+use anyhow::{anyhow, Result};
 use http_body_util::Full;
 use hyper::body::Bytes;
-
 use hyper::server::conn::http1;
 use hyper::service::service_fn;
 use hyper::{Request, Response};
 use ics::{components::Property, Event, ICalendar};
 use regex::Regex;
 use std::collections::HashMap;
-use std::convert::Infallible;
 use std::io::BufReader;
 use tokio::net::TcpListener;
 use url::Url;
@@ -29,38 +28,47 @@ struct Args {
     dedup: bool,
 }
 
-async fn filter_ical(
-    _: Request<hyper::body::Incoming>,
-) -> Result<Response<Full<Bytes>>, Infallible> {
+async fn filter_ical(_: Request<hyper::body::Incoming>) -> Result<Response<Full<Bytes>>> {
     let args = Args::parse();
 
-    let source = reqwest::get(args.source).await.unwrap();
-    let source = source.text().await.unwrap();
+    let source = reqwest::get(args.source).await?;
+    let source = source.text().await?;
 
     let reader = ical::IcalParser::new(BufReader::new(source.as_bytes()));
 
     let mut blacklist = HashMap::<String, Vec<Regex>>::new();
     args.blacklist.iter().for_each(|b| {
-        let mut split = b.splitn(2, '=');
-        let key = split.next().unwrap();
-        let value = split.next().unwrap();
+        let (key, value) = match b.splitn(2, '=').collect_tuple() {
+            Some((k, r)) => (k, r),
+            None => {
+                println!("Invalid blacklist rule: {}", b);
+                return;
+            }
+        };
 
-        let value = Regex::new(value).unwrap();
+        let regex = match Regex::new(value) {
+            Ok(regex) => regex,
+            Err(e) => {
+                println!("Invalid blacklist regex: {}\n{:?}", value, e);
+                return;
+            }
+        };
 
         if let Some(v) = blacklist.get_mut(key) {
-            v.push(value);
+            v.push(regex);
         } else {
-            blacklist.insert(key.to_string(), vec![value]);
+            blacklist.insert(key.to_string(), vec![regex]);
         }
     });
 
-    let calendar = reader.into_iter().next().unwrap().unwrap();
+    let calendar = reader.into_iter().next();
+    let calendar = calendar.ok_or(anyhow!("No calendar found"))??; // Unwrap calendar, returning anyhow error on failure
     let events = calendar.events.iter().filter(|e| {
         e.properties.iter().all(|p| {
             if let Some(blacklist) = blacklist.get(&p.name) {
                 blacklist
                     .iter()
-                    .all(|r| !r.is_match(&p.value.clone().unwrap()))
+                    .all(|r| !r.is_match(&p.value.clone().unwrap_or("".to_string())))
             } else {
                 true
             }
@@ -77,9 +85,9 @@ async fn filter_ical(
                     .collect();
 
                 (
-                    props.get("DTSTART").unwrap().clone(),
-                    props.get("DTEND").unwrap().clone(),
-                    props.get("SUMMARY").unwrap().clone(),
+                    props.get("DTSTART").cloned(),
+                    props.get("DTEND").cloned(),
+                    props.get("SUMMARY").cloned(),
                 )
             })
             .collect::<Vec<_>>()
@@ -87,49 +95,65 @@ async fn filter_ical(
         events.collect::<Vec<_>>()
     };
 
-    let cal_version = calendar
+    let cal_props: HashMap<_, _> = calendar
         .properties
         .iter()
-        .find(|p| p.name == "VERSION")
-        .unwrap()
-        .value
-        .clone()
-        .unwrap();
-    let prod_id = calendar
-        .properties
-        .iter()
-        .find(|p| p.name == "PRODID")
-        .unwrap()
-        .value
-        .clone()
-        .unwrap();
+        .map(|p| (p.name.clone(), p.value.clone()))
+        .collect();
+
+    let cal_version = cal_props
+        .get("VERSION")
+        .cloned()
+        .flatten()
+        .ok_or(anyhow!("No VERSION found on calendar"))?;
+    let prod_id = cal_props
+        .get("PRODID")
+        .cloned()
+        .flatten()
+        .ok_or(anyhow!("No PRODID found on calendar"))?;
 
     let mut output_calendar = ICalendar::new(cal_version, prod_id);
 
-    events.iter().for_each(|e| {
-        let props: HashMap<String, String> = e
-            .properties
-            .iter()
-            .map(|p| (p.name.clone(), p.value.clone().unwrap_or("".to_string())))
-            .collect();
+    let results: Vec<Result<_>> = events
+        .iter()
+        .map(|e| {
+            let props: HashMap<String, String> = e
+                .properties
+                .iter()
+                .map(|p| (p.name.clone(), p.value.clone().unwrap_or("".to_string())))
+                .collect();
 
-        let mut output_event = Event::new(
-            props.get("UID").unwrap().clone(),
-            props.get("DTSTAMP").unwrap().clone(),
-        );
+            let mut output_event = Event::new(
+                props
+                    .get("UID")
+                    .cloned()
+                    .ok_or(anyhow!("No UID found on event"))?,
+                props
+                    .get("DTSTAMP")
+                    .cloned()
+                    .ok_or(anyhow!("No DTSTAMP found on event"))?,
+            );
 
-        e.properties.iter().for_each(|p| {
-            output_event.push(Property::new(
-                p.name.clone(),
-                p.value.clone().unwrap_or("".to_string()),
-            ));
-        });
+            e.properties.iter().for_each(|p| {
+                output_event.push(Property::new(
+                    p.name.clone(),
+                    p.value.clone().unwrap_or("".to_string()),
+                ));
+            });
 
-        output_calendar.add_event(output_event);
-    });
+            output_calendar.add_event(output_event);
+
+            Ok(())
+        })
+        .collect();
+    for result in results {
+        if let Err(e) = result {
+            println!("Error while filtering events: {:?}", e);
+        }
+    }
 
     let mut buf = Vec::new();
-    output_calendar.write(&mut buf).unwrap();
+    output_calendar.write(&mut buf)?;
 
     let mut res = Response::new(Full::from(buf));
     res.headers_mut().insert(
